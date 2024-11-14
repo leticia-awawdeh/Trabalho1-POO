ddefb06520d9397657c74618a91463ecd49d8449
vitin <vitin@DESKTOP-VO9LQMK>
2024-11-13 22:36:32 -0300

    alterações desejadas realizadas

--- src/Livro.java
+++ src/Livro.java
@@ -1,67 +1,61 @@
 
 public class Livro {
-	private String titulo;
+	String titulo;
 	String autor;
 	Generos genero;
-	int anoPublicacao;
-	private int numExemplares;
-
-	// TODO: colocar na classe ENUM
-	public enum Generos {
-	        ROMANCE,
-	        FICCAO,
-	        BIOGRAFIA,
-	        CONTO,
-	        AUTOBIOGRAFIA
-	}
+	int anoPubli;
+	int numExemplares;
 
 	
-	public Livro(String Titulo,String Autor,Generos genero,int AnoPubli ,int NumExemplares) {
-		this.titulo = Titulo;
-        this.autor = Autor;
+	public Livro(String titulo,String autor,Generos genero,int anoPubli ,int numExemplares) {
+		this.titulo = titulo;
+        this.autor = autor;
         this.genero = genero;
-        this.anoPublicacao = AnoPubli;
-        this.numExemplares = NumExemplares;
+        this.anoPubli = anoPubli;
+        this.numExemplares =numExemplares;
 		
 	}
 
-	public boolean emprestar(int quantidaeEmprestada){
-		if(this.numExemplares > 0){
-			this.setNumExemplares(this.getNumExemplares() - quantidaeEmprestada);
-			return true;
-		} else {
-			System.out.println("Não há exemplares disponíveis para o livro: " + titulo);
-			return false;
-		}
-	}
+	public boolean emprestar(int quantidadeEmprestada) {
+        if (this.numExemplares >= quantidadeEmprestada) {
+            this.SetNumExemplares(this.getNumExemplares() - quantidadeEmprestada);
+            return true;
+        } else {
+            System.out.println("Não há exemplares disponíveis para o livro: " + titulo);
+            return false;
+        }
+    }
 
-	//TODO: Colocar os Seters
 	 public String getTitulo() {
 	        return titulo;
 	    }
-
+	 public void setTitulo(String titulo) {
+	        this.titulo = titulo;
+	    }
 	    public String getAutor() {
 	        return autor;
 	    }
-
+	    public void setAutor(String autor)
+	    {
+	    	  this.autor=autor;
+	    }
 	    public Generos getGenero() {
 	        return genero;
 	    }
-
-	    public int getAnoPublicacao() {
-	        return anoPublicacao;
+	    public void setGenero(Generos genero)
+	    {
+	    	  this.genero=genero;
 	    }
-
-		public void setNumExemplares(int exemplaresDisponiveis){
-			this.numExemplares = exemplaresDisponiveis;
-		}
+	    public int getAnoPubli() {
+	        return anoPubli;
+	    }
+	    public void SetAnoPubli(int anoPubli) {
+	    	this.anoPubli=anoPubli;
+	    }
 	    public int getNumExemplares() {
 	        return numExemplares;
 	    }
+	    public void SetNumExemplares(int numExemplares){
+	    	this.numExemplares=numExemplares;
+	    }
 	}
-	
-
-	
-
-
-
